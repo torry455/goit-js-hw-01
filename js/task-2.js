09db9fc08282d67decf6d5ead57f77922c381548
Viktoriia Kucher <142267781+torry455@users.noreply.github.com>
2023-10-25 23:05:50 +0300

    completed task-2

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+function getShippingMessage(country, price, delieveryFee) {
+    let totalPrice = price + delieveryFee;
+
+    const msg = `Shipping to ${country} will cost ${totalPrice} credits`;
+
+    return msg;
+}
+
+console.log(getShippingMessage("Australia", 120, 50)); 
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20));
