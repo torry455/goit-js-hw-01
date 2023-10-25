b4b9481a70f7c3d8e6c1cb37bc1eb03a6a2d8743
Viktoriia Kucher <142267781+torry455@users.noreply.github.com>
2023-10-25 22:54:03 +0300

    completed task-1

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function makeTransaction(quantity, pricePerDroid) {
+    let totalPrice = quantity * pricePerDroid;
+
+    const msg = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+
+    return msg;
+}
+
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500)); 
+
+
