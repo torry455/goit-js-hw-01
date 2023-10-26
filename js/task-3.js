9a583983f5893baa1fa0da4bf6466b5ceb0b15a7
Viktoriia Kucher <142267781+torry455@users.noreply.github.com>
2023-10-26 13:40:35 +0300

    corrected task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
+
 function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
-
-    return `${contentWidth + 2 * paddingWidth + 2 * borderWidth}`;
-}
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+    return contentWidth + paddingWidth * 2 + borderWidth * 2;
+  };
+  
+  console.log(getElementWidth("50px", "8px", "4px"));
+  console.log(getElementWidth("60px", "12px", "8.5px"));
+  console.log(getElementWidth("200px", "0px", "0px"));
